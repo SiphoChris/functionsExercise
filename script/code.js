980b68803186084b95523752c9a579b3a156dbd2
SiphoChris <siphonkebe@gmail.com>
2024-05-23 11:17:24 +0200

    submission && git push git add . && git commit -m submission

---
+++ script/code.js
@@ -0,0 +1,23 @@
+//activity one
+
+const greet = (someValue) => {
+    console.log(`Hello ${someValue}`)
+}
+greet('world')
+
+//activity two
+
+function multiply(num1, num2){
+    console.log(num1 * num2)
+}
+
+multiply(2, 2)
+
+//activity three
+
+const tableOfFive = function(someValue){
+    result = someValue * 5
+    return result
+}
+
+console.log(tableOfFive(3))
